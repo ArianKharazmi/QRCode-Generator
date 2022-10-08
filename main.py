6191611af434285c1dded9c58353330f82bee6b1
Arian Kharazmi <100003892+ArianKharazmi@users.noreply.github.com>
2022-10-07 22:36:04 -0400

    Release v1.9
    Release v1.9 changes:
    
    ~Cleaned up code (mostly)
    ~Added OpenCV / Camera Capabilities for Reverse QR Code Search
    ~Added Basic Selenium for future updates around automating Reverse QRCode Search automation

--- main.py
+++ main.py
@@ -2,20 +2,61 @@
 import requests
 import qrcode
 from datetime import date
+import sys
+import numpy as np
+import cv2
+import selenium
+
+Generate = qrcode
+Reverse = cv2
 
 
-today = date.today()
-print("QRCode Generation completed on " + str(today) + ". Try it out!")
+print("Welcome to the QRCode Tool! Which option will you choose?")
+answer = input(
+    "Generate QR Code or use Reverse QR Code Search? **Type of the two choices:** (Generate/Reverse) ").lower()
+if answer == Generate:
+    today = date.today()
+    print("QRCode Generation completed on " + str(today) + ". Try it out!")
+
+    # Input link or text entry into qrcode.make section
+    qr = qrcode.QRCode(version=1,
+                       error_correction=qrcode.constants.ERROR_CORRECT_L,
+                       box_size=50,
+                       border=2)
+
+    # Edit your text entry/URL
+    qr.make(fit=True)
+    qr.add_data("Your QR Code")
+    # Customization Options
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("QRCodeIMG.PNG")
+
+if answer == Reverse:
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+#today = date.today()
+#print("QRCode Generation completed on " + str(today) + ". Try it out!")
 
 # Input link or text entry into qrcode.make section
-qr = qrcode.QRCode(version = 1,
-                   error_correction = qrcode.constants.ERROR_CORRECT_L,
-                   box_size = 50,
-                   border = 2)
+#qr = qrcode.QRCode(version = 1,
+                   #error_correction = qrcode.constants.ERROR_CORRECT_L,
+                   #box_size = 50,
+                   #border = 2)
 
 # Edit your text entry/URL
-qr.make(fit = True)
-qr.add_data("Your QR Code")
+#qr.make(fit = True)
+#qr.add_data("Your QR Code")
 # Customization Options
-img = qr.make_image(fill_color="black", back_color = "white")
-img.save("QRCodeIMG.PNG")
+#img = qr.make_image(fill_color="black", back_color = "white")
+#img.save("QRCodeIMG.PNG")
